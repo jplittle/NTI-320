d0e7de1eb2ed1f3cdacb1c778bf5298238800f5d
jplittle <43659489+jplittle@users.noreply.github.com>
2019-04-04 19:15:57 -0700

    Create nrpe_install.sh

---
+++ nrpe_install.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+apt-get -y install nagios-nrpe-server nagios-plugins
+sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.142.0.15/g' /etc/nagios/nrpe.cfg
+systemctl restart nagios-nrpe-server

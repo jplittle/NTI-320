f6c53f25bc89a642f74b4f712381a9a04da9d549
jplittle <43659489+jplittle@users.noreply.github.com>
2019-04-18 18:52:21 -0700

    Update nrpe_install.sh

--- nrpe_install.sh
+++ nrpe_install.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
 yum -y install nagios-nrpe-server nagios-plugins nrpe
+yum install -y nagios-nrpe-server nagios-plugins nrpe nagios-plugins-load nagios-plugins-ping nagios-plugins-disk nagios-plugins-http nagios-plugins-procs wget
 systemctl enable nrpe
 systemctl start nrpe
 sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.150.0.2/g' /etc/nagios/nrpe.cfg

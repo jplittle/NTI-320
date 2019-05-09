0f23c11534e0e622e80617b113ae3cae2d09d416
jplittle <43659489+jplittle@users.noreply.github.com>
2019-05-09 08:52:01 -0700

    Create for_loop.sh

---
+++ for_loop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a );  do 
+
+    echo $servername;
+
+    serverip=$( gcloud compute instances list | grep $servername | awk '{print $4}');
+
+    echo $serverip ;
+
+    bash scp_to_nagios.sh $servername $serverip
+
+done
+
+gcloud compute ssh --zone us-east4-c nicolebade@nagios-a --command='sudo systemctl restart nagios'

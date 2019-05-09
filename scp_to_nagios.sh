aa82613e5c67d9431dceffcb6e7cfedbb1407a71
jplittle <43659489+jplittle@users.noreply.github.com>
2019-05-09 08:54:38 -0700

    Create scp_to_nagios.sh

---
+++ scp_to_nagios.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+bash generate_config.sh $1 $2
+
+gcloud compute scp --zone us-east4-c $1.cfg nicolebade@nagios-a:/etc/nagios/servers
+
+# Note: I had to add user nicolebade to group nagios using usermod -a -G nagios nicolebade
+
+# I also had to chmod 775 /etc/nagios/servers
+
+gcloud compute ssh --zone us-east4-c nicolebade@nagios-a --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'

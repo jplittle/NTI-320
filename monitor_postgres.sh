5cf2b02c31cb2136ea10a4170f8b483f6af3843e
jplittle <43659489+jplittle@users.noreply.github.com>
2019-05-30 19:35:17 -0700

    Update monitor_postgres.sh

--- monitor_postgres.sh
+++ monitor_postgres.sh
@@ -1,1 +1,23 @@
-!#/bin/bash
+#!/bin/bash
+# This is a script meant to let us test NRPE
+# plugins we write
+cd /usr/lib/systemd/system
+status=$(systemctl is-active postgresql.service)    # Determines if postgresql.service is inactive or active
+status1=$(systemctl is-disabled postgresql.service)  # Determines if postgresql.service is disabled or enabled
+
+if [ $status == "active" ]; then
+    echo "STATUS:OK POSTGRES IS RUNNING"
+    exit 0;
+    
+  elif [ $status == "inactive" ]; then
+    echo "STATUS:CRITICAL POSTGRES IS DOWN"
+    exit 2;
+    
+  elif [ $status1 == "disabled" ] ; then
+    echo "STATUS:WARNING POSTGRES IS DISABLED"
+    exit 1;
+    
+else
+   echo "STATUS:UNKNOWN"
+   exit 3;
+f

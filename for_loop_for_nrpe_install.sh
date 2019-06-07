3ee9f448ce8d7316bc888f24fd522fe712534d1f
jplittle <43659489+jplittle@users.noreply.github.com>
2019-06-06 20:00:16 -0700

    Update for_loop_for_nrpe_install.sh

--- for_loop_for_nrpe_install.sh
+++ for_loop_for_nrpe_install.sh
@@ -1,6 +1,6 @@
 for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a );  do 
 
-    gcloud compute ssh --zone us-east4-c jamesplittle25@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/jplittle/NTI-320/master/nrpe_install.sh && sudo bash nrpe_install.sh sudo wget https://raw.githubusercontent.com/jplittle/NTI-320/master/for_'
+    gcloud compute ssh --zone us-east4-c jamesplittle25@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/jplittle/NTI-320/master/nrpe_install.sh && sudo bash nrpe_install.sh sudo wget https://raw.githubusercontent.com/jplittle/NTI-320/master/loop_add_yum_repo.sh && sudo bash loop_add_yum_repo.sh'
 
 done
 #sudo yum -y install wget && 

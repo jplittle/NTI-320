744736c72641e7fe6fca4d2c6772e9aa3e4ed35d
jplittle <43659489+jplittle@users.noreply.github.com>
2019-06-06 18:53:57 -0700

    Update rpm-build-server.sh

--- rpm-build-server.sh
+++ rpm-build-server.sh
@@ -6,10 +6,10 @@ echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 cd ~/rpmbuild/SOURCES
 git clone https://github.com/nic-instruction/NTI-320.git
 cd NTI-320/
-cp NTI-320/rpm-info/hello_world_from_source/helloworld-0.1.tar.gz .
-cp NTI-320/rpm-info/hello_world_from_source/helloworld.sh .
-cp NTI-320/rpm-info/hello_world_from_source/hello.spec .
-mv hello.spec ../SPECS
+#cp NTI-320/rpm-info/hello_world_from_source/helloworld-0.1.tar.gz . ### day of we will change
+#cp NTI-320/rpm-info/hello_world_from_source/helloworld.sh .
+#cp NTI-320/rpm-info/hello_world_from_source/hello.spec .
+#mv hello.spec ../SPECS
 cd ..
 rpmbuild -v -bb --clean SPECS/hello.spec
 #ls -l RPMS/x86_64/helloworld-0.1-1.el7.x86_64.rpm

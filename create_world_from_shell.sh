b81478f23ac2559a1731bb5ff5334ec17179cf62
jplittle <43659489+jplittle@users.noreply.github.com>
2019-06-06 17:54:31 -0700

    Update create_world_from_shell.sh

--- create_world_from_shell.sh
+++ create_world_from_shell.sh
@@ -5,6 +5,6 @@
 
 
 
-##where it is in the file system will control what it does
+##where it is in the file system will control what it doesd
 
 ##it's placement will automatically run if in /etc/name.d/

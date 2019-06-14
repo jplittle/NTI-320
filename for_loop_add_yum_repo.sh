42cf9c7b024f0ada3de1a3f5d410b3f4a079735d
jplittle <43659489+jplittle@users.noreply.github.com>
2019-06-13 21:05:38 -0700

    Update for_loop_add_yum_repo.sh

--- for_loop_add_yum_repo.sh
+++ for_loop_add_yum_repo.sh
@@ -6,14 +6,14 @@
 ip="10.150.0.4"                                                    # Please replace with your repo server IP
 
 #vim /etc/yum.repos.d/epel.repo
-echo "[nti-320]
+echo '[nti-320]
 name=Extra Packages for Centos from NTI-320 7 - $basearch
 #baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
 # Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
 baseurl=http://$ip/centos/7/extras/x86_64/Packages/
 enabled=1
 gpgcheck=0
-" >> /etc/yum.repos.d/NTI-320.repo                                  
+' >> /etc/yum.repos.d/NTI-320.repo                                  
                                                                     # Now that the repo is added, list all repos and make sure
                                                                     # it shows up without error
 yum repolist

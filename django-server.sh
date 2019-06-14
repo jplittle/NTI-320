79ac0e3220db62ec318777a251c54fc2ed7a5982
jplittle <43659489+jplittle@users.noreply.github.com>
2019-06-13 18:21:53 -0700

    Create django-server.sh

---
+++ django-server.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+   
+    sudo su  
+    yum -y install python-pip
+    pip install virtualenv
+    pip install --upgrade pip
+    ##mkdir and cd into
+    mkdir /opt/django
+    cd /opt/django/
+    yum install python36
+    virtualenv -p python36 django
+    ##still in 2.7.5 && need to source to 3.6
+    python --version
+    source django/bin/activate
+    pip install django
+    django-admin --version    
+    ##start project
+    django-admin startproject project1
+    ##change ownership from root to a user
+    chown -R <user> django 
+    chown -R <user> project1
+    su - <user>  
+    ##activate django install and run server
+    source /opt/django/django/bin/activate
+    /opt/django/project1/manage.py runserver 0.0.0.0:8000&
+    
+sudo yum update -y && yum install -y rsyslog 	#CentOS 7
+sudo systemctl start rsyslog
+sudo systemctl enable rsyslog
+#on the client
+#add to end of file
+echo "*.* @@rsyslog-server-final:514" >> /etc/rsyslog.conf

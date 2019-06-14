902d87b9a1f3b28c5460a6e99f5e91236d82ab1d
jplittle <43659489+jplittle@users.noreply.github.com>
2019-06-13 18:48:05 -0700

    Update rpm-build-server.sh

--- rpm-build-server.sh
+++ rpm-build-server.sh
@@ -3,14 +3,14 @@ yum -y install rpm-build make gcc git
 mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS} 
 cd ~/
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
-cd ~/rpmbuld/SOURCES
+
 # cd ~/rpmbuild/SOURCES
-# git clone https://github.com/nic-instruction/NTI-320.git
-# cd NTI-320/
-#cp NTI-320/rpm-info/hello_world_from_source/helloworld-0.1.tar.gz . ### day of we will change
-#cp NTI-320/rpm-info/hello_world_from_source/helloworld.sh .
-#cp NTI-320/rpm-info/hello_world_from_source/hello.spec .
-#mv hello.spec ../SPECS
+git clone https://github.com/nic-instruction/custom-nrpe-2019
+cd custom-nrpe-2019/
+cp custom-nrpe-2019/nti-320-plugins-0.1.tar.gz .
+cp custom-nrpe-2019/plugins/check_ldaps_cert_expiry.sh .
+cp custom-nrpe-2019/nti-320-plugins.spec .
+mv nti-320-plugins.spec ../SPECS
 # cd ..
 # rpmbuild -v -bb --clean SPECS/hello.spec
 #ls -l RPMS/x86_64/helloworld-0.1-1.el7.x86_64.rpm
